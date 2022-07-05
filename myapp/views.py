ce76d6158e5953c4754510716f7ecd01b36b3fa1
seun0123 <seunpark04@gmail.com>
2022-07-05 12:59:01 +0900

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import BlogForm
+from django.utils import timezone
+from .models import Blog
 
 # Create your views here.
+def index(request):
+    return render(request, 'main.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form = form.save(commmit=False)
+            form.pub_date = timezone.now()
+            form.save()
+            return redirect('main')
+    else:
+        form = BlogForm
+        return render(request, 'write.html', {'form' : form})
+
+def read(request):
+    blogs = Blog.object
+    return render(request, 'read.html', {'blogs':blogs})
+
+def read(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    return render(request, 'read.html', {'blog':blog})
